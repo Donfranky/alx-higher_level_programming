571f2673db3e58ba31ef1fa62bb66968bf4ae191
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2023-03-13 04:28:34 -0700

    print int

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+
+#endif /* LISTS_H */
